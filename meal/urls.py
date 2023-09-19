6bf7c7ae8675c00ea2066fd3604cd3a931a37933
RenoLee1 <yinuol5@student.unimelb.edu.au>
2023-09-20 02:30:51 +1000

    Delete

--- meal/urls.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-from django.urls import path
-# Create your views here.
-app_name = 'meal'
-
-urlpatterns = [
-    path('', TemplateView.as_view(template_name="meal/index.html"))
-]
